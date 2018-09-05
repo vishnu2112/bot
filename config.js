de67fa28bc04021c46322f8dbcd87c7246b281d7
vishn_u <vp493137@gmail.com>
2018-09-05 20:57:28 +0530

    adding tokens to it

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+   consumer_key:        'AeSUM75quKpNlAWXnNbcDCQ8V',
+   consumer_secret:      'QbTMmbREPkaFo5hQGoROwe1R1zcoRwKi1Ce7u4EfClE8BelsKh',
+   access_token:         '1035175128273838080-YYsoHD5TmZ5R24nSAzoI7c6n78GanA',
+   access_token_secret:   'TWcgfTQ2c7OUjBamrpIsElqpPIz7ZjYzej73kzPY6GsPW',
+}
+    
